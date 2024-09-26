51ac0c39ad0d3937e0cd5cbc451495e533e5e113
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-09-26 17:27:50 +0700

    Bộ lọc trạng thái

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -3,11 +3,22 @@ import Task from "../models/task.model"
 
 // [GET] /tasks/
 export const index = async (req: Request, res: Response) => {
-    const tasks = await Task.find({
+    interface Find {
+        deleted: boolean,
+        status?: string
+    }
+    
+    const find: Find = {
         deleted: false
-    })
+    }
+    
+    if (req.query.status) {
+        find.status = req.query.status.toString()
+    }
 
-    if (!tasks) {
+    const tasks = await Task.find(find)
+
+    if (tasks.length == 0) {
         return res.json({
             code: 400, 
             message: "No tasks found."
