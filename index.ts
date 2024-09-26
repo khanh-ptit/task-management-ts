cd90e613a6261bb5547cc6bdc6f0532f7ce469b2
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-09-26 16:15:23 +0700

    Lấy ra chi tiết công việc

--- index.ts
+++ index.ts
@@ -18,6 +18,19 @@ app.get("/tasks", async (req: Request, res: Response): Promise<void> => {
     })
 })
 
+app.get("/tasks/detail/:id", async (req: Request, res: Response) => {
+    const id = req.params.id
+    // console.log(id)
+    const task = await Task.findOne({
+        _id: id,
+        deleted: false
+    })
+    
+    res.json({
+        task: task
+    })
+})
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
