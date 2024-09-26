b7dbd2e45d989461740e371f6c305bd13c39c727
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-09-26 17:13:13 +0700

    Cấu hình MVC

---
+++ api/v1/controllers/task.controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express"
+import Task from "../models/task.model"
+
+// [GET] /tasks/
+export const index = async (req: Request, res: Response) => {
+    const tasks = await Task.find({
+        deleted: false
+    })
+
+    if (!tasks) {
+        return res.json({
+            code: 400, 
+            message: "No tasks found."
+        })
+    }
+
+    res.json({
+        tasks: tasks
+    })
+}
+
+// [GET] /tasks/detail/:id
+export const detail = async (req: Request, res: Response) => {
+    const id = req.params.id
+    try {
+        // console.log(id)
+        const task = await Task.findOne({
+            _id: id,
+            deleted: false
+        })
+
+        res.json({
+            code: 200,
+            task: task
+        })
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Có lỗi xảy ra!"
+        })
+    }
+}
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import express, { Express, Request, Response } from "express"
+import express, { Express } from "express"
 import * as database from "./config/database"
 import dotenv from "dotenv"
-import Task from "./models/task.model"
+import mainV1Routes from "./api/v1/routes/index.route"
 
 dotenv.config()
 database.connect()
@@ -9,27 +9,7 @@ database.connect()
 const app: Express = express()
 const port: number | string = process.env.PORT || 3000
 
-app.get("/tasks", async (req: Request, res: Response): Promise<void> => {
-    const tasks = await Task.find({
-        deleted: false
-    })
-    res.json({
-        tasks: tasks
-    })
-})
-
-app.get("/tasks/detail/:id", async (req: Request, res: Response) => {
-    const id = req.params.id
-    // console.log(id)
-    const task = await Task.findOne({
-        _id: id,
-        deleted: false
-    })
-    
-    res.json({
-        task: task
-    })
-})
+mainV1Routes(app)
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
