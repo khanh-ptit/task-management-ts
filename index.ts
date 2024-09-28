fef32235f6a150150213dd44720593962db1bc00
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-09-28 10:05:06 +0700

    Cài đặt CORS

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express"
 import * as database from "./config/database"
 import dotenv from "dotenv"
 import mainV1Routes from "./api/v1/routes/index.route"
+import cors from "cors";
 
 dotenv.config()
 database.connect()
@@ -10,6 +11,7 @@ const app: Express = express()
 const port: number | string = process.env.PORT || 3000
 
 app.use(express.json());
+app.use(cors())
 
 mainV1Routes(app)
 
